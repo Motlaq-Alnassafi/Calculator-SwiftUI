270f4725c0300224173b278d50075b91821049f9
Motlaq <motlaqalnesafi@gmail.com>
2023-09-27 12:26:36 +0300

    Initial Commit

---
+++ Calculator-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculator-SwiftUI
+//
+//  Created by Eng. Motlaq Alnassafi on 27/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
